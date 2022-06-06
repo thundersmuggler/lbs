ea1f58fe48e643bd73ff8f6957cedd0e4e826888
Felipe Sette <frss777@gmail.com>
2022-06-06 14:00:46 -0300

    escrevendo a interpretar

--- interpretar.c
+++ interpretar.c
@@ -8,24 +8,25 @@ unsigned char interpretar_funcao (char *entrada) {
 	unsigned char j = 0;
 	unsigned char k = 0;
 	char atual [50];
-	char e1 [10];
-	char e2 [10];
-	char e3 [10];
-	char e4 [10];
-	char e5 [10];
-	while (entrada [i] != 'e') {
-		while (entrada [i] != '\n') {
+	// não sei se usarei var [50] ou e1, e2, e3, e4, e5
+	int var [5];
+	char i_var; // indice de var
+	while (entrada [i] != 'e') { // ler até o fim da função ("end")
+		while (entrada [i] != '\n') { // ler até o fim da linha ('\n')
 			atual [j] = entrada [i];
 			++ j;
 			++ i;
 		}
-		atual
-		i = 0;
-		j = 0;
-		// agora temos a linha atual em ATUAL
-		// agora particionaremos a linha atual em elementos
-		while (atual [i] != '\0') {
+                // agora temos a linha atual em ATUAL
+                // agora particionaremos a linha atual em elementos
+		for (int k = 0; k < j; k++) {
+			if (atual [k] == 'v') {
+				++ k;
+				i_var = atual [k] - '0';
+				var [i_var] = 
+			}
 		}
+		j = 0;
 		++ i;
 	}
 	return saida
